5f906abbf47787a4c80b5f561c5884a4220f6622
Chandrakant Mishra <cm237575@gmail.com>
2024-10-12 11:31:43 +0530

    Initial commit

---
+++ Demo.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const unitInputs = document.querySelectorAll('input[name="unit"]');
+  const totalDisplay = document.getElementById("total");
+
+  unitInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+      const selectedValue = input.value;
+      totalDisplay.textContent = ` Total: $${selectedValue}.00 USD`;
+    });
+  });
+});
